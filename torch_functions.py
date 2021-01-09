75078a1a203289d762fd795da2057058fb4f59d6
bbttboy <46322472+bbttboy@users.noreply.github.com>
2021-01-09 19:07:01 +0800

    Update torch_functions.py

--- torch_functions.py
+++ torch_functions.py
@@ -119,10 +119,15 @@ class NormalizationLayer(torch.nn.Module):
   """Class for normalization layer."""
   def __init__(self, normalize_scale=1.0, learn_scale=True):
     super(NormalizationLayer, self).__init__()
+    # normalize_scale-->缩放参数？
     self.norm_s = float(normalize_scale)
     if learn_scale:
       self.norm_s = torch.nn.Parameter(torch.FloatTensor((self.norm_s,)))
 
   def forward(self, x):
+    # torch.norm中，'p'表示范数形式，不设置p表示默认为2范数
+    # dim表示在哪个维度上进行计算，例如shape=(2,4)在dim=1上进行计算，输出是一个(2)的形式
+    # keepdim=True表示维持原有Shape的形态，同上，输出是一个(2,1)的形式，如果在dim=0上计算，输出是(1,4)形式
+    # expand_as(x)表示对tensor进行广播扩充成同x的形式，即把(2,1)或(1,4)广播扩充成(2,4)
     features = self.norm_s * x / torch.norm(x, dim=1, keepdim=True).expand_as(x)
     return features
